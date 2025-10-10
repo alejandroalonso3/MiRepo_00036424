64ec911ce46ab4c4181d5454dbc61be1b2d867a1
alejandroalonso3 <00036424@uca.edu.sv>
2025-10-10 14:14:56 -0600

    ejercicio labo 5

---
+++ Labo5/ejercicio.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int numero;
+cout << "ingrese un numero: ";
+cin >> numero;
+
+for (int i=1; i<=20; i++ ) {
+
+cout << numero << "x" << i << "=" << numero * i << endl;
+}
+
+}
+
